ba13267ec29e826dc05bbeb64a6eb167cfcd94fc
sclipse <karlo.versic@gmail.com>
2025-01-10 18:29:50 +0100

    update footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="border-t dark:border-neutral-800">
       <div className="container mx-auto px-4 py-8 flex flex-col justify-between items-center">
         <p className="text-sm text-muted-foreground mb-4 sm:mb-0">
-          Built with ❤️ using Next.js, Supabase, and ShadCN/UI.
+          Built with{" "}
+          <Link
+            className="hover:text-primary"
+            href="https://www.linkedin.com/in/karloversic/"
+          >
+            ❤️
+          </Link>{" "}
+          using{" "}
+          <Link className="hover:text-primary" href="https://nextjs.org/">
+            Next.js
+          </Link>
+          ,{" "}
+          <Link className="hover:text-primary" href="https://nextjs.org/">
+            Supabase
+          </Link>
+          , and{" "}
+          <Link className="hover:text-primary" href="https://nextjs.org/">
+            ShadCN/UI
+          </Link>
+          .
         </p>
         {/*<nav className="flex gap-4">*/}
         {/*  <Link*/}
